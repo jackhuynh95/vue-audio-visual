5d7e9910460f2712d069c77b65ab93d0426694b0
Jack Huynh <kietdien88@gmail.com>
2023-04-12 10:10:42 +0700

    feat: ignore support vue-component

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue'
-import AVBars from '@/components/AVBars.vue'
-import AVCircle from '@/components/AVCircle.vue'
-import AVLine from '@/components/AVLine.vue'
-import AVWaveform from '@/components/AVWaveform.vue'
-import AVMedia from '@/components/AVMedia.vue'
+// import AVBars from '@/components/AVBars.vue'
+// import AVCircle from '@/components/AVCircle.vue'
+// import AVLine from '@/components/AVLine.vue'
+// import AVWaveform from '@/components/AVWaveform.vue'
+// import AVMedia from '@/components/AVMedia.vue'
 import { useAVBars } from '@/composables/useAVBars'
 import { useAVCircle } from '@/composables/useAVCircle'
 import { useAVLine } from '@/composables/useAVLine'
@@ -12,20 +12,20 @@ import { useAVMedia } from '@/composables/useAVMedia'
 
 const AVPlugin = {
   install(app: App) {
-    app.component('av-bars', AVBars)
-    app.component('av-circle', AVCircle)
-    app.component('av-line', AVLine)
-    app.component('av-waveform', AVWaveform)
-    app.component('av-media', AVMedia)
+    // app.component('av-bars', AVBars)
+    // app.component('av-circle', AVCircle)
+    // app.component('av-line', AVLine)
+    // app.component('av-waveform', AVWaveform)
+    // app.component('av-media', AVMedia)
   }
 }
 
 export {
-  AVBars,
-  AVCircle,
-  AVLine,
-  AVWaveform,
-  AVMedia,
+  // AVBars,
+  // AVCircle,
+  // AVLine,
+  // AVWaveform,
+  // AVMedia,
   useAVBars,
   useAVCircle,
   useAVLine,
